199978bfa3c6cfdfe1f6334b2a7c7f749b7be45e
Roman Dolgiy <roman@bravetstudio.com>
2011-06-27 22:11:21 +0300

    Initial commit

---
+++ gae_main.py
@@ -0,0 +1,13 @@
+import sys
+from os.path import dirname, abspath, join
+
+
+PROJECT_DIR = join(abspath(dirname(__file__)), "testapp")
+
+if PROJECT_DIR not in sys.path or sys.path.index(project_dir) > 0:
+    while PROJECT_DIR in sys.path:
+        sys.path.remove(PROJECT_DIR)
+    sys.path.insert(0, PROJECT_DIR)
+
+import djangoappengine.main.main
+djangoappengine.main.main.main()
